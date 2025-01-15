6ba1d63bd72e13f1c087cfafa2f1056bce4e504f
Jara86 <jarouschka@gmail.com>
2025-01-16 00:53:39 +0100

    clean up code

--- src/Index.js
+++ src/Index.js
@@ -4,7 +4,7 @@ function generatePoem(event) {
     let instructionsInput = document.querySelector("#topic");
     let apiKey = "ta004a4a3b736802do35c5853a06aff7";
     let prompt = `User instructions generate a poem ${instructionsInput.value}`;
-    let context = `You are a wise old wizard poet who loves to write short poems. Your mission is to generate a 4 line poem  about the following: ${instructionsInput.value}`;
+    let context = `You are a wise old wizard poet who loves to write short poems. Your mission is to generate a 4 line poem in basic HTML format. Make sure to follow the user instructions exactly.`;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
     console.log("Generating poem...");
@@ -15,12 +15,18 @@ function generatePoem(event) {
 }
 
 function displayPoem(response) {
+    let cleanPoem = response.data.answer.replace(/'''html|```html/g, "").trim();
+    
     let typewriter = new Typewriter("#response-poem", {
-        strings: [response.data.answer],
+        strings: [cleanPoem],
         autoStart: true,
         delay: 100,
         cursor: "",
         loop: false
+    });
+
+    typewriter.start().callFunction(() => {
+        document.querySelector("#response-poem").innerHTML = cleanPoem;
     });
 }
 
